f990eb49ce64b1e9d0c9e257844e6f1f84b3c174
idohyeon <ehgus5026@naver.com>
2024-06-01 02:11:40 +0900

    -Member

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,6 +2,15 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 일대다 단방향 매핑 단점
+ * 엔티티가 관리하는 외래 키가 다른 테이블에 있음.
+ * 연관관계 관리를 위해 추가로 UPDATE SQL 실행
+ * 웬만하면 다대일 양방향 매핑 사용(단방향이 일단 최우선)
+ */
 @Entity
 public class Member {
 
@@ -13,6 +22,11 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+    // 일대다
+    /*@OneToMany
+    @JoinColumn(name = "MEMBER_ID")
+    private List<Order> orders = new ArrayList<>();*/
 
     public Long getId() {
         return id;
