09bb404c29559db1348b6b6294285044e79077bc
idohyeon <ehgus5026@naver.com>
2024-06-01 02:13:36 +0900

    -Order (@ManyToOne(Member), @OneToOne(Delivery), @OneToMany(OrderItem))

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -6,12 +6,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 일대다 단방향 매핑 단점
- * 엔티티가 관리하는 외래 키가 다른 테이블에 있음.
- * 연관관계 관리를 위해 추가로 UPDATE SQL 실행
- * 웬만하면 다대일 양방향 매핑 사용(단방향이 일단 최우선)
- */
 @Entity
 @Table(name = "ORDERS") // DB에 ORDER가 예약어로 걸려 있어서
 public class Order {
@@ -29,14 +23,20 @@ public class Order {
     @JoinColumn(name = "MEMBER_ID") // 쓰지 않으면 조인용 테이블로 중간 테이블이 하나 더 추가돼서 운영에도 어려움이 있음.
     private Member member;
 
+    // 배송과 주문은 일대일 관계
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
     // 양방향 (값만 가져오기 용)
+    // 다대다를 중간 테이블인 orderItem을 만들어 일대다로 풀어냄
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
     // 일대다 상황에서 역방향으로 읽기 전용으로 쓰려고 할 때인데 야매 방법임.
     /*@ManyToOne
-    @JoinColumn(insertable = false, updatable = false)
-    private OrderItem orderItem;*/
+    @JoinColumn(name = "MEMBER_ID", insertable = false, updatable = false)
+    private Member member*/
 
     private LocalDateTime orderDate;
 
