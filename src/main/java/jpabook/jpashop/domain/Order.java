1d5419b83b77dbdbe3b64a1f9bab7b92067e6824
idohyeon <ehgus5026@naver.com>
2024-06-02 02:38:07 +0900

    -Order (@manytoone(Member), @OnetoOne(Delivery), @OneToMany(OrderItem))

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,18 +19,21 @@ public class Order extends BaseEntity {
     private Long memberId;*/
 
     // 외래키가 주인
-    @ManyToOne // 단방향
+    @ManyToOne(fetch = FetchType.LAZY) // 단방향
     @JoinColumn(name = "MEMBER_ID") // 쓰지 않으면 조인용 테이블로 중간 테이블이 하나 더 추가돼서 운영에도 어려움이 있음.
     private Member member;
 
     // 배송과 주문은 일대일 관계
-    @OneToOne
+    // 지연로딩, order를 생성해서 오더를 저장하면 자동으로 딜리버리도 저장
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
     // 양방향 (값만 가져오기 용)
     // 다대다를 중간 테이블인 orderItem을 만들어 일대다로 풀어냄
-    @OneToMany(mappedBy = "order")
+    // 연관관계의 자식 클래스에 cascade 걸기
+    // order를 생성해서 오더를 저장(persist)를 하면 자동으로 orderItems도 저장
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     // 일대다 상황에서 역방향으로 읽기 전용으로 쓰려고 할 때인데 야매 방법임.
